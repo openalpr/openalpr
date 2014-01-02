773d6e123fcca3c3a2bd9aef6686cb28fcec673a
Matt Hill <matthill@gmail.com>
2014-01-02 16:27:43 -0600

    Added openalpr utility programs

---
+++ src/misc_utilities/benchmark.cpp
@@ -0,0 +1,361 @@
+/*
+ * Copyright (c) 2013 New Designs Unlimited, LLC
+ * Opensource Automated License Plate Recognition [http://www.openalpr.com]
+ * 
+ * This file is part of OpenAlpr.
+ * 
+ * OpenAlpr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License 
+ * version 3 as published by the Free Software Foundation 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+ #include "opencv2/highgui/highgui.hpp"
+ #include "opencv2/imgproc/imgproc.hpp"
+
+ #include <iostream>
+ #include <fstream>
+ #include <stdio.h>
+#include <sys/stat.h>
+#include <numeric>      // std::accumulate
+
+#include "alpr_impl.h"
+
+//#include "stage1.h"
+//#include "stage2.h"
+//#include "stateidentifier.h"
+//#include "utility.h"
+#include "support/filesystem.h"
+
+ using namespace std;
+ using namespace cv;
+
+// Given a directory full of lp images (named [statecode]#.png) crop out the alphanumeric characters.
+// These will be used to train the OCR
+ 
+void outputStats(vector<double> datapoints);
+ 
+int main( int argc, const char** argv )
+{
+   string country;
+   string benchmarkName;
+   string inDir;
+   string outDir;
+   Mat frame;
+   
+   
+  //Check if user specify image to process
+  if(argc == 5)
+  {
+        country = argv[1];
+	benchmarkName = argv[2];
+        inDir = argv[3];
+        outDir = argv[4];
+	  
+	
+  }else{
+      printf("Use:\n\t%s [country] [benchmark name] [img input dir] [results output dir]\n",argv[0]);
+      printf("\tex: %s us speed ./speed/usimages ./speed\n",argv[0]);
+      printf("\n");
+      printf("\ttest names are: speed, segocr, detection\n\n" );
+      return 0;
+  }   
+  
+  
+  if (DirectoryExists(inDir.c_str()) == false)
+  {
+      printf("Input dir does not exist\n");
+      return 0;
+  }
+  if (DirectoryExists(outDir.c_str()) == false)
+  {
+      printf("Output dir does not exist\n");
+      return 0;
+  }
+  
+  
+  vector<string> files = getFilesInDir(inDir.c_str());
+  sort( files.begin(), files.end(), stringCompare );
+  
+  
+  if (benchmarkName.compare("segocr") == 0)
+  {
+    Config* config = new Config(country);  
+    config->debugOff();
+    
+    OCR* ocr = new OCR(config);
+    
+    
+    for (int i = 0; i< files.size(); i++)
+    {
+      if (hasEnding(files[i], ".png"))
+      {
+	string fullpath = inDir + "/" + files[i];
+
+	frame = imread( fullpath.c_str() );
+	resize(frame, frame, Size(config->ocrImageWidthPx, config->ocrImageHeightPx));
+	
+	Rect plateCoords;
+	plateCoords.x = 0;
+	plateCoords.y = 0;
+	plateCoords.width = frame.cols;
+	plateCoords.height = frame.rows;
+	
+	char statecode[3];
+	statecode[0] = files[i][0];
+	statecode[1] = files[i][1];
+	statecode[2] = '\0';
+	string statecodestr(statecode);
+	
+	CharacterRegion charRegion(frame, config);
+	
+	if (abs(charRegion.getTopLine().angle) > 4)
+	{
+	  // Rotate image:
+	  Mat rotated(frame.size(), frame.type());
+	  Mat rot_mat( 2, 3, CV_32FC1 );
+	  Point center = Point( frame.cols/2, frame.rows/2 );
+	  
+	  rot_mat = getRotationMatrix2D( center, charRegion.getTopLine().angle, 1.0 );
+	  warpAffine( frame, rotated, rot_mat, frame.size() );
+	
+	  rotated.copyTo(frame);
+	}
+
+	
+	
+	CharacterSegmenter charSegmenter(frame, charRegion.thresholdsInverted(), config);
+	ocr->performOCR(charSegmenter.getThresholds(), charSegmenter.characters);
+	ocr->postProcessor->analyze(statecode, 25);
+		  
+	cout << files[i] << "," << statecode << "," << ocr->postProcessor->bestChars << endl;
+
+	
+	imshow("Current LP", frame);
+	waitKey(5);
+	  
+	
+      }
+	
+    }
+    
+    delete config;
+    delete ocr;
+  }
+  else if (benchmarkName.compare("detection") == 0)
+  {
+    Config config(country);
+    RegionDetector plateDetector(&config);
+    
+    for (int i = 0; i< files.size(); i++)
+    {
+      if (hasEnding(files[i], ".png"))
+      {
+	string fullpath = inDir + "/" + files[i];
+	frame = imread( fullpath.c_str() );
+	
+	vector<Rect> regions = plateDetector.detect(frame);
+	
+	imshow("Current LP", frame);
+	waitKey(5);
+	  
+	
+      }
+	
+    }
+  }
+  else if (benchmarkName.compare("speed") == 0)
+  {
+      // Benchmarks speed of region detection, plate analysis, and OCR
+  
+    timespec startTime;
+    timespec endTime;
+    
+    Config config(country);
+    config.debugOff();
+    
+    AlprImpl alpr(country);
+    alpr.config->debugOff();
+    alpr.setDetectRegion(true);
+    
+    RegionDetector plateDetector(&config);
+    StateIdentifier stateIdentifier(&config);
+    OCR ocr(&config);
+    
+    vector<double> endToEndTimes;
+    vector<double> regionDetectionTimes;
+    vector<double> stateIdTimes;
+    vector<double> lpAnalysisPositiveTimes;
+    vector<double> lpAnalysisNegativeTimes;
+    vector<double> ocrTimes;
+    vector<double> postProcessTimes;
+    
+    
+    for (int i = 0; i< files.size(); i++)
+    {
+      if (hasEnding(files[i], ".png"))
+      {
+	cout << "Image: " << files[i] << endl;
+	
+	string fullpath = inDir + "/" + files[i];
+	frame = imread( fullpath.c_str() );
+	
+	
+	getTime(&startTime);
+	alpr.recognize(frame);
+	getTime(&endTime);
+	double endToEndTime = diffclock(startTime, endTime);
+	cout << " -- End to End recognition time: " << endToEndTime << "ms." << endl;
+	endToEndTimes.push_back(endToEndTime);
+	
+	getTime(&startTime);
+	vector<Rect> regions = plateDetector.detect(frame);
+	getTime(&endTime);
+	
+	double regionDetectionTime = diffclock(startTime, endTime);
+	cout << " -- Region detection time: " << regionDetectionTime << "ms." << endl;
+	regionDetectionTimes.push_back(regionDetectionTime);
+	
+	for (int z = 0; z < regions.size(); z++)
+	{
+	  getTime(&startTime);
+	  char temp[5];
+	  stateIdentifier.recognize(frame, regions[z], temp);
+	  getTime(&endTime);
+	  double stateidTime = diffclock(startTime, endTime);
+	  cout << "\tRegion " << z << ": State ID time: " << stateidTime << "ms." << endl;
+	  stateIdTimes.push_back(stateidTime);
+	  
+	  getTime(&startTime);
+	  LicensePlateCandidate lp(frame, regions[z], &config);
+	  lp.recognize();
+	  getTime(&endTime);
+	  double analysisTime = diffclock(startTime, endTime);
+	  cout << "\tRegion " << z << ": Analysis time: " << analysisTime << "ms." << endl;
+	  
+	  if (lp.confidence > 10)
+	  {
+	    lpAnalysisPositiveTimes.push_back(analysisTime);
+	    
+	    
+	    getTime(&startTime);
+	    ocr.performOCR(lp.charSegmenter->getThresholds(), lp.charSegmenter->characters);
+	    getTime(&endTime);
+	    double ocrTime = diffclock(startTime, endTime);
+	    cout << "\tRegion " << z << ": OCR time: " << ocrTime << "ms." << endl;
+	    ocrTimes.push_back(ocrTime);
+	  
+	    getTime(&startTime);
+	    ocr.postProcessor->analyze("", 25);
+	    getTime(&endTime);
+	    double postProcessTime = diffclock(startTime, endTime);
+	    cout << "\tRegion " << z << ": PostProcess time: " << postProcessTime << "ms." << endl;
+	    postProcessTimes.push_back(postProcessTime);
+	  }
+	  else
+	  {
+	    lpAnalysisNegativeTimes.push_back(analysisTime);
+	  }
+	}
+      
+	waitKey(5);
+	  
+      }
+	
+    }
+        
+    cout << endl << "---------------------" << endl;
+    
+    
+    cout << "End to End Time Statistics:" << endl;
+    outputStats(endToEndTimes);
+    cout << endl;
+    
+    cout << "Region Detection Time Statistics:" << endl;
+    outputStats(regionDetectionTimes);
+    cout << endl;
+    
+    cout << "State ID Time Statistics:" << endl;
+    outputStats(stateIdTimes);
+    cout << endl;
+    
+    cout << "Positive Region Analysis Time Statistics:" << endl;
+    outputStats(lpAnalysisPositiveTimes);
+    cout << endl;
+    
+    cout << "Negative Region Analysis Time Statistics:" << endl;
+    outputStats(lpAnalysisNegativeTimes);
+    cout << endl;
+    
+    cout << "OCR Time Statistics:" << endl;
+    outputStats(ocrTimes);
+    cout << endl;
+    
+    cout << "Post Processing Time Statistics:" << endl;
+    outputStats(postProcessTimes);
+    cout << endl;
+  }
+  else if (benchmarkName.compare("endtoend") == 0)
+  {
+    Alpr alpr(country);
+    alpr.setDetectRegion(true);
+    
+    ofstream outputdatafile;
+    
+    outputdatafile.open("results.txt");
+    
+    for (int i = 0; i< files.size(); i++)
+    {
+      if (hasEnding(files[i], ".png"))
+      {
+	string fullpath = inDir + "/" + files[i];
+	frame = imread( fullpath.c_str() );
+	
+	vector<uchar> buffer;
+	imencode(".bmp", frame, buffer );
+	
+	vector<AlprResult> results = alpr.recognize(buffer);
+	
+	outputdatafile  << files[i] << ": ";
+	for (int z = 0; z < results.size(); z++)
+	{
+	   outputdatafile  << results[z].bestPlate.characters << ", ";
+	}
+	outputdatafile  << endl;
+	
+	imshow("Current LP", frame);
+	waitKey(5);
+	  
+	
+      }
+	
+    }
+    
+    outputdatafile.close();
+  }
+  
+}
+
+void outputStats(vector<double> datapoints)
+{
+  double sum = std::accumulate(datapoints.begin(), datapoints.end(), 0.0);
+  double mean = sum / datapoints.size();
+
+  std::vector<double> diff(datapoints.size());
+  std::transform(datapoints.begin(), datapoints.end(), diff.begin(),
+		std::bind2nd(std::minus<double>(), mean));
+  double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+  double stdev = std::sqrt(sq_sum / datapoints.size());
+
+  cout << "\t" << datapoints.size() << " samples, avg: " << mean << "ms,  stdev: " << stdev << endl;
+
+}
+
+
---
+++ src/misc_utilities/classifychars.cpp
@@ -0,0 +1,355 @@
+/*
+ * Copyright (c) 2013 New Designs Unlimited, LLC
+ * Opensource Automated License Plate Recognition [http://www.openalpr.com]
+ * 
+ * This file is part of OpenAlpr.
+ * 
+ * OpenAlpr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License 
+ * version 3 as published by the Free Software Foundation 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+ #include "opencv2/highgui/highgui.hpp"
+ #include "opencv2/imgproc/imgproc.hpp"
+
+ #include <iostream>
+ #include <stdio.h>
+#include <sys/stat.h>
+
+#include "regiondetector.h"
+#include "licenseplatecandidate.h"
+#include "stateidentifier.h"
+#include "utility.h"
+#include "support/filesystem.h"
+#include "ocr.h"
+
+ using namespace std;
+ using namespace cv;
+
+// Given a directory full of lp images (named [statecode]#.png) crop out the alphanumeric characters.
+// These will be used to train the OCR
+
+const int LEFT_ARROW_KEY = 81;
+const int RIGHT_ARROW_KEY = 83;
+const int SPACE_KEY = 32;
+const int ENTER_KEY = 10;
+const int ESCAPE_KEY = 27;
+ 
+const int DOWN_ARROW_KEY = 84;
+const int UP_ARROW_KEY= 82;
+const int DASHBOARD_COLUMNS = 3;
+
+void showDashboard(vector<Mat> images, vector<bool> selectedImages, int selectedIndex);
+vector<char> showCharSelection(Mat image, vector<Rect> charRegions, string state);
+ 
+int main( int argc, const char** argv )
+{
+   
+   string inDir;
+   string outDir;
+   Mat frame;
+   
+   
+  //Check if user specify image to process
+  if(argc == 3)
+  {
+        inDir = argv[1];
+	outDir = argv[2];
+	  
+	
+  }else{
+      printf("Use:\n\t%s indirectory outdirectory\n",argv[0]);
+      printf("Ex: \n\t%s ./pics/  ./out  \n",argv[0]);
+      return 0;
+  }   
+  
+  
+  if (DirectoryExists(outDir.c_str()) == false)
+  {
+      printf("Output dir does not exist\n");
+      return 0;
+  }
+  
+  cout << "Usage: " << endl;
+  cout << "\tn		-- Next plate" << endl;
+  cout << "\tp		-- Previous plate" << endl;
+  cout << "\ts		-- Save characters" << endl;
+  cout << "\t<- and ->	-- Cycle between images" << endl;
+  cout << "\tEnt/space	-- Select plate" << endl;
+  cout << endl;
+  cout << "Within a plate" << endl;
+  cout << "\t<- and ->		-- Cycle between characters" << endl;
+  cout << "\t[0-9A-Z]		-- Identify a character (saves the image)" << endl;
+  cout << "\tESC/Ent/Space	-- Back to plate selection" << endl;
+  
+  Config* config = new Config("eu");
+  OCR ocr(config);
+  
+  if (DirectoryExists(inDir.c_str()))
+  {
+      vector<string> files = getFilesInDir(inDir.c_str());
+      
+      sort( files.begin(), files.end(), stringCompare );
+      
+      for (int i = 0; i< files.size(); i++)
+      {
+	if (hasEnding(files[i], ".png") || hasEnding(files[i], ".jpg"))
+	{
+	  string fullpath = inDir + "/" + files[i];
+	  cout << fullpath << endl;
+	  frame = imread( fullpath.c_str() );
+	  resize(frame, frame, Size(config->ocrImageWidthPx, config->ocrImageHeightPx));
+	  
+	  imshow ("Original", frame);
+	  
+	  
+	  char statecode[3];
+	  statecode[0] = files[i][0];
+	  statecode[1] = files[i][1];
+	  statecode[2] = '\0';
+	  string statecodestr(statecode);
+	  
+	  CharacterRegion regionizer(frame, config);
+	  
+	  if (abs(regionizer.getTopLine().angle) > 4)
+	  {
+	    // Rotate image:
+	    Mat rotated(frame.size(), frame.type());
+	    Mat rot_mat( 2, 3, CV_32FC1 );
+	    Point center = Point( frame.cols/2, frame.rows/2 );
+	    
+	    rot_mat = getRotationMatrix2D( center, regionizer.getTopLine().angle, 1.0 );
+	    warpAffine( frame, rotated, rot_mat, frame.size() );
+	  
+	    rotated.copyTo(frame);
+	  }
+	  
+	  CharacterSegmenter charSegmenter(frame, regionizer.thresholdsInverted(), config);
+	  
+	  //ocr.cleanCharRegions(charSegmenter.thresholds, charSegmenter.characters);
+	  
+	  ocr.performOCR(charSegmenter.getThresholds(), charSegmenter.characters);
+	  ocr.postProcessor->analyze(statecodestr, 25);
+	  cout << "OCR results: " << ocr.postProcessor->bestChars << endl;
+	  
+	  
+	  vector<bool> selectedBoxes(charSegmenter.getThresholds().size());
+	  for (int z = 0; z < charSegmenter.getThresholds().size(); z++)
+	    selectedBoxes[z] = false;
+	  
+	  int curDashboardSelection = 0;
+	  
+	  vector<char> humanInputs(charSegmenter.characters.size());
+	  
+	  for (int z = 0; z < charSegmenter.characters.size(); z++)
+	    humanInputs[z] = ' ';
+	  
+	  showDashboard(charSegmenter.getThresholds(), selectedBoxes, 0);
+	  
+	  
+	  
+	  char waitkey = (char) waitKey(50);
+	  
+	  while (waitkey != 'n' && waitkey != 'p')	 // Next image
+	  {
+	    if (waitkey == LEFT_ARROW_KEY) // left arrow key
+	    {
+	      if (curDashboardSelection > 0)
+		curDashboardSelection--;
+	      showDashboard(charSegmenter.getThresholds(), selectedBoxes, curDashboardSelection);
+	    }
+	    else if (waitkey == RIGHT_ARROW_KEY) // right arrow key
+	    {
+	      if (curDashboardSelection < charSegmenter.getThresholds().size() - 1)
+		curDashboardSelection++;
+	      showDashboard(charSegmenter.getThresholds(), selectedBoxes, curDashboardSelection);
+	    }
+	    else if (waitkey == DOWN_ARROW_KEY)
+	    {
+		if (curDashboardSelection + DASHBOARD_COLUMNS <= charSegmenter.getThresholds().size() - 1)
+		  curDashboardSelection += DASHBOARD_COLUMNS;
+	      showDashboard(charSegmenter.getThresholds(), selectedBoxes, curDashboardSelection);
+	    }
+	    else if (waitkey == UP_ARROW_KEY)
+	    {
+		if (curDashboardSelection - DASHBOARD_COLUMNS >= 0)
+		  curDashboardSelection -= DASHBOARD_COLUMNS;
+	      showDashboard(charSegmenter.getThresholds(), selectedBoxes, curDashboardSelection);
+	    }
+	    else if (waitkey == ENTER_KEY)
+	    {
+	      vector<char> tempdata = showCharSelection(charSegmenter.getThresholds()[curDashboardSelection], charSegmenter.characters, statecodestr);
+	      for (int c = 0; c < charSegmenter.characters.size(); c++)
+		humanInputs[c] = tempdata[c];
+	    }
+	    else if (waitkey == SPACE_KEY)
+	    {
+	      selectedBoxes[curDashboardSelection] = !selectedBoxes[curDashboardSelection];
+	      showDashboard(charSegmenter.getThresholds(), selectedBoxes, curDashboardSelection);
+	    }
+	    else if (waitkey == 's' || waitkey == 'S')
+	    {
+	      bool somethingSelected = false;
+	      bool chardataTagged = false;
+	      for (int c = 0; c < charSegmenter.getThresholds().size(); c++)
+	      {
+		if (selectedBoxes[c])
+		{
+		    somethingSelected = true;
+		    break;
+		}
+	      }
+	      for (int c = 0; c < charSegmenter.characters.size(); c++)
+	      {
+		if (humanInputs[c] != ' ')
+		{
+		    chardataTagged = true;
+		    break;
+		}
+	      }
+	      // Save
+	      if (somethingSelected && chardataTagged)
+	      {
+		
+		for (int c = 0; c < charSegmenter.characters.size(); c++)
+		{
+		  if (humanInputs[c] == ' ')
+		    continue;
+		  
+		  for (int t = 0; t < charSegmenter.getThresholds().size(); t++)
+		  {
+		    if (selectedBoxes[t] == false)
+		      continue;
+		    
+		    stringstream filename;
+		    Mat cropped = charSegmenter.getThresholds()[t](charSegmenter.characters[c]);
+		    filename << outDir << "/" << humanInputs[c] << "-" << t << "-" << files[i];
+		    imwrite(filename.str(), cropped);
+		    cout << "Writing char image: " << filename.str() << endl;
+		  }
+		}
+	      }
+	      else if (somethingSelected == false)
+		cout << "Did not select any boxes" << endl;
+	      else if (chardataTagged == false)
+		cout << "You have not tagged any characters" << endl;
+	    }
+	      
+	    waitkey = (char) waitKey(50);
+	    
+	  }
+
+	  if (waitkey == 'p')
+	    i = i - 2;
+	  if (i < -1)
+	    i = -1;
+	    
+	  
+	}
+	 
+      }
+  }
+  
+}
+
+
+void showDashboard(vector<Mat> images, vector<bool> selectedImages, int selectedIndex)
+{
+  vector<Mat> vecCopy;
+  
+  if (selectedIndex < 0)
+    selectedIndex = 0;
+  if (selectedIndex >= images.size())
+    selectedIndex = images.size() -1;
+  
+  for (int i = 0; i < images.size(); i++)
+  {
+    Mat imgCopy(images[i].size(), images[i].type());
+    images[i].copyTo(imgCopy);
+    cvtColor(imgCopy, imgCopy, CV_GRAY2BGR);
+    if (i == selectedIndex)
+    {
+      rectangle(imgCopy, Point(1,1), Point(imgCopy.size().width - 1, imgCopy.size().height -1), Scalar(0, 255, 0), 1);
+    }
+    if (selectedImages[i] == true)
+    {
+      rectangle(imgCopy, Point(2,2), Point(imgCopy.size().width - 2, imgCopy.size().height -2), Scalar(255, 0, 0), 1);
+    }
+    
+    vecCopy.push_back(imgCopy);
+  }
+  
+  Mat dashboard = drawImageDashboard(vecCopy, vecCopy[0].type(), DASHBOARD_COLUMNS);
+  
+  imshow("Selection dashboard", dashboard);
+}
+
+vector<char> showCharSelection(Mat image, vector<Rect> charRegions, string state)
+{
+    int curCharIdx = 0;
+    
+    vector<char> humanInputs(charRegions.size());
+    for (int i = 0; i < charRegions.size(); i++)
+      humanInputs[i] = (char) SPACE_KEY;
+    
+    
+    char waitkey = (char) waitKey(50);
+    while (waitkey != ENTER_KEY && waitkey != ESCAPE_KEY)
+    {
+      Mat imgCopy(image.size(), image.type());
+      image.copyTo(imgCopy);
+      cvtColor(imgCopy, imgCopy, CV_GRAY2BGR);
+      
+      rectangle(imgCopy, charRegions[curCharIdx], Scalar(0, 255, 0), 1);
+      
+      imshow("Character selector", imgCopy);
+      
+      if (waitkey == LEFT_ARROW_KEY)
+	curCharIdx--;
+      else if (waitkey == RIGHT_ARROW_KEY )
+	curCharIdx++;
+      else if ((waitkey >= '0' && waitkey <= '9') || (waitkey >= 'a' && waitkey <= 'z') || waitkey == SPACE_KEY)
+      {
+	// Save the character to disk
+	humanInputs[curCharIdx] = toupper((char) waitkey);
+	curCharIdx++;
+	
+	if (curCharIdx >= charRegions.size())
+	{
+	  waitkey = (char) ENTER_KEY;
+	  break;
+	}
+      }
+      
+      if (curCharIdx < 0)
+	curCharIdx = 0;
+      if (curCharIdx >= charRegions.size())
+	curCharIdx = charRegions.size() -1;
+      
+      waitkey = (char) waitKey(50);
+    }
+  
+    if (waitkey == ENTER_KEY)
+    {
+	// Save all the inputs
+	for (int i = 0; i < charRegions.size(); i++)
+	{
+	  if (humanInputs[i] != (char) SPACE_KEY)
+	    cout << "Tagged " << state << " char code: '" << humanInputs[i] << "' at char position: " << i << endl;
+	}
+	
+    }
+  
+    destroyWindow("Character selector");
+    
+    
+    return humanInputs;
+}
