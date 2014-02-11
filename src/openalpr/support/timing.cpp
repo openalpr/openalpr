09db5395897c104f2a2d5e5adc8592ef4000c3b9
Kristians Vebers <kristians.vebers@gmail.com>
2014-02-11 14:10:06 +0200

    Added timing support for Mac OS X

--- src/openalpr/support/timing.cpp
+++ src/openalpr/support/timing.cpp
@@ -1,5 +1,10 @@
 #include "timing.h"
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
 #ifdef WINDOWS
 
 
@@ -28,7 +33,19 @@ timespec diff(timespec start, timespec end);
 
 void getTime(timespec* time)
 {
+
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+	clock_serv_t cclock;
+	mach_timespec_t mts;
+	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+	clock_get_time(cclock, &mts);
+	mach_port_deallocate(mach_task_self(), cclock);
+	time->tv_sec = mts.tv_sec;
+	time->tv_nsec = mts.tv_nsec;
+#else
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, time);
+#endif
+
 }
 double diffclock(timespec time1,timespec time2)
 {
