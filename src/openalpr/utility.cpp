fee77e9aea5973490031ee1f83b05b52ab2d895f
Kristians Vebers <kristians.vebers@gmail.com>
2014-02-19 10:09:38 +0200

    Reversed segfault fix as it was already fixed

--- src/openalpr/utility.cpp
+++ src/openalpr/utility.cpp
@@ -177,7 +177,7 @@ double median(int array[], int arraySize)
   }
   
   std::sort(&array[0], &array[arraySize]);
-  return arraySize % 2 ? array[arraySize / 2] : (array[max(0, arraySize / 2 - 1)] + array[arraySize / 2]) / 2;
+  return arraySize % 2 ? array[arraySize / 2] : (array[arraySize / 2 - 1] + array[arraySize / 2]) / 2;
 }
 
 
