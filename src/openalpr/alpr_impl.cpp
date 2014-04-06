8052e9fdc432ea6da02962f5790b4cd2bda01299
Matt Hill <matthill@gmail.com>
2014-04-06 12:22:06 -0500

    Moved image pause to always take effect if enabled

--- src/openalpr/alpr_impl.cpp
+++ src/openalpr/alpr_impl.cpp
@@ -138,14 +138,14 @@ std::vector<AlprResult> AlprImpl::recognize(cv::Mat img)
     displayImage(config, "Main Image", img);
     cv::waitKey(1);
     
-    if (config->debugPauseOnFrame)
-    {
-      // Pause indefinitely until they press a key
-      while ((char) cv::waitKey(50) == -1)
-      {}
-    }
   }
-
+  
+  if (config->debugPauseOnFrame)
+  {
+    // Pause indefinitely until they press a key
+    while ((char) cv::waitKey(50) == -1)
+    {}
+  }
   
   return dispatcher.getRecognitionResults();
 }
