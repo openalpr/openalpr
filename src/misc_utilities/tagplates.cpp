f3ec4fd554f7cb00d5a1400afbc133d838011548
Matt Hill <matthill@gmail.com>
2014-10-06 22:21:33 -0400

    Changed aspect

--- src/misc_utilities/tagplates.cpp
+++ src/misc_utilities/tagplates.cpp
@@ -64,7 +64,7 @@ static int xPos1 = 0;
 static int yPos1 = 0;
 static int xPos2 = 0;
 static int yPos2 = 0;
-const float ASPECT_RATIO = 4.33333;
+const float ASPECT_RATIO = 1.404;
 
 static bool rdragging = false;
 static int rDragStartX = 0;
