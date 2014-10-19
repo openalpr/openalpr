2896d2186880070d2a03c00c5d5a8a6a92138494
Matt Hill <matthill@gmail.com>
2014-10-19 15:54:25 -0400

    Removed unused function prototype from characterAnalysis

--- src/openalpr/textdetection/characteranalysis.h
+++ src/openalpr/textdetection/characteranalysis.h
@@ -63,7 +63,6 @@ class CharacterAnalysis
     void filterByOuterMask(TextContours& textContours);
 
     std::vector<cv::Point> getCharArea(LineSegment topLine, LineSegment bottomLine);
-    std::vector<cv::Point> getBestVotedLines(cv::Mat img, TextContours textContours);
     void filterBetweenLines(cv::Mat img, TextContours& textContours, std::vector<TextLine> textLines );
 
     bool verifySize(cv::Mat r, float minHeightPx, float maxHeightPx);
