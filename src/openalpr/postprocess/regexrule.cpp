dafab6c1927b9f77fc41896e3cf10fa22f8f59d2
Matt Hill <matthill@gmail.com>
2015-04-28 20:35:57 -0400

    Added regexrule class

---
+++ src/openalpr/postprocess/regexrule.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   regexrule.cpp
+ * Author: mhill
+ * 
+ * Created on April 28, 2015, 6:28 AM
+ */
+
+#include "regexrule.h"
+
+namespace alpr
+{
+  
+  RegexRule::RegexRule() {
+  }
+
+
+  RegexRule::~RegexRule() {
+  }
+  
+}
+
---
+++ src/openalpr/postprocess/regexrule.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   regexrule.h
+ * Author: mhill
+ *
+ * Created on April 28, 2015, 6:28 AM
+ */
+
+#ifndef OPENALPR_REGEXRULE_H
+#define	OPENALPR_REGEXRULE_H
+
+namespace alpr
+{
+  class RegexRule {
+  public:
+    RegexRule();
+    virtual ~RegexRule();
+  private:
+
+  };
+}
+
+#endif	/* OPENALPR_REGEXRULE_H */
+
