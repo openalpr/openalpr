7061db554833640c098e140c4830b7eb40089087
Matt Hill <matthill@openalpr.com>
2015-08-05 18:47:29 -0400

    Cleaned up Python bindings.  Moved to openalpr package and added import in __init__

---
+++ src/bindings/python/openalpr/__init__.py
@@ -0,0 +1,1 @@
+from openalpr import Alpr
---
+++ src/bindings/python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='openalpr',
+      version='1.0',
+      description='OpenALPR Python Bindings',
+      author='Matt Hill',
+      author_email='matthill@openalpr.com',
+      url='http://www.openalpr.com/',
+      packages=['openalpr']
+     )
