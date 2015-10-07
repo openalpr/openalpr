b19bd8c2d6e393b59c1be613e7c82dd3c7150401
Peter Rekdal Sunde <peter.sunde@gmail.com>
2015-10-07 12:19:56 +0200

    https://github.com/openalpr/openalpr/commit/ec24d1d1474e2acecd3dcf48df37d980e125141d

--- src/bindings/csharp/openalpr-net/types-net.h
+++ src/bindings/csharp/openalpr-net/types-net.h
@@ -12,7 +12,8 @@ namespace openalprnet
 		public enum class AlprDetectorTypeNet : int {
 			DetectorLbpCpu = alpr::DETECTOR_LBP_CPU,
 			DetectorLbpGpu = alpr::DETECTOR_LBP_GPU,
-			DetectorLbpMorphCpu = alpr::DETECTOR_MORPH_CPU
+			DetectorLbpMorphCpu = alpr::DETECTOR_MORPH_CPU,
+			DetectorLbpOpencl = alpr::DETECTOR_LBP_OPENCL
 		};
 	}
 }
