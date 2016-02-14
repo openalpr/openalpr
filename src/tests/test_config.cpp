9d6fad0c2e8ad39b9d1f0d784075b84d49462f6a
Matt Hill <matthill@openalpr.com>
2016-02-14 15:58:17 -0500

    Added unit tests for config

--- src/tests/test_config.cpp
+++ src/tests/test_config.cpp
@@ -8,6 +8,7 @@
 #include <cstdlib>
 #include "catch.hpp"
 #include "config.h"
+#include "alpr.h"
 
 using namespace std;
 using namespace alpr;
@@ -18,36 +19,49 @@ Config get_config(std::string countries)
   return config;
 }
 
-//TEST_CASE( "Loading Countries", "[Config]" ) {
-//
-//  REQUIRE( get_config("us,eu").loaded_countries.size() == 2 );
-//
-//  REQUIRE( get_config("us").loaded_countries.size() == 1 );
-//
-//  REQUIRE( get_config("eu").loaded_countries.size() == 1 );
-//
-//  REQUIRE( get_config("us, eu").loaded_countries.size() == 2 );
-//
-//  REQUIRE( get_config("us, eu, au, kr").loaded_countries.size() == 4 );
-//
-//  REQUIRE( get_config("us , eu").loaded_countries.size() == 2 );
-//
-//  REQUIRE( get_config("us,eu,").loaded_countries.size() == 2 );
-//
-//  REQUIRE( get_config(",,us,eu,").loaded_countries.size() == 2 );
-//
-//}
+TEST_CASE( "Loading Countries", "[Config]" ) {
+
+  REQUIRE( get_config("us,eu").loaded_countries.size() == 2 );
+
+  REQUIRE( get_config("us").loaded_countries.size() == 1 );
+
+  REQUIRE( get_config("eu").loaded_countries.size() == 1 );
+
+  REQUIRE( get_config("us, eu").loaded_countries.size() == 2 );
+
+  REQUIRE( get_config("us, eu, au, kr").loaded_countries.size() == 4 );
+
+  REQUIRE( get_config("us , eu").loaded_countries.size() == 2 );
+
+  REQUIRE( get_config("us,eu,").loaded_countries.size() == 2 );
+
+  REQUIRE( get_config(",,us,eu,").loaded_countries.size() == 2 );
+
+}
 
 
-//TEST_CASE( "Modifying Countries", "[Config]" )
-//{
-//  Config config("us,eu", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data/");
-//
-//  REQUIRE(config.ocrLanguage == "lus");
-//
-//  config.setCountry("eu");
-//  REQUIRE(config.ocrLanguage == "leu");
-//
-//  config.setCountry("us");
-//  REQUIRE(config.ocrLanguage == "lus");
-//}
+TEST_CASE( "Modifying Countries", "[Config]" )
+{
+  Config config("us,eu", OPENALPR_TESTING_CONFIG_PATH, OPENALPR_TESTING_RUNTIME_DIR);
+
+  REQUIRE(config.ocrLanguage == "lus");
+
+  config.setCountry("eu");
+  REQUIRE(config.ocrLanguage == "leu");
+
+  config.setCountry("us");
+  REQUIRE(config.ocrLanguage == "lus");
+}
+
+TEST_CASE( "Reloading Countries", "[Config]" )
+{
+  Alpr alpr("us", OPENALPR_TESTING_CONFIG_PATH, OPENALPR_TESTING_RUNTIME_DIR);
+
+  REQUIRE(alpr.getConfig()->ocrLanguage == "lus");
+
+  alpr.setCountry("eu");
+  
+  REQUIRE(alpr.getConfig()->ocrLanguage == "leu");
+
+
+}
