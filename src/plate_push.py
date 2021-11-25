07373b8b18c8d4486a614733d7d37ca9a81eceb6
Christian Clauss <cclauss@me.com>
2021-11-25 11:31:16 +0100

    Use print() function in both Python 2 and Python 3

--- src/plate_push.py
+++ src/plate_push.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 import beanstalkc
 
 beanstalk = beanstalkc.Connection(host='localhost', port=11300)
@@ -8,6 +10,6 @@ beanstalk.watch('alpr')
 
 job = beanstalk.reserve()
 
-print job.body
+print(job.body)
 
 job.delete()
