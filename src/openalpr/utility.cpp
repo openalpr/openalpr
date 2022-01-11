e1d136d9c30b4e17287e92a1762c2eac667890e9
Hamid Ebadi <hamid.ebadi@gmail.com>
2022-01-11 13:58:14 +0100

    Not openning cv window

--- src/openalpr/utility.cpp
+++ src/openalpr/utility.cpp
@@ -112,11 +112,13 @@ namespace alpr
 
   void displayImage(Config* config, string windowName, cv::Mat frame)
   {
+  /*
     if (config->debugShowImages)
     {
       imshow(windowName, frame);
       cv::waitKey(5);
     }
+    */
   }
 
   vector<Mat> produceThresholds(const Mat img_gray, Config* config)
