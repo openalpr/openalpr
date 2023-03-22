afe52dcac6e4c314a5a4c6d07fefc13a995bf120
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-03-22 20:35:52 -0300

    f

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -21,18 +21,18 @@ def main():
     while not os.listdir(file_path):
         time.sleep(0.5)
         
-    # Process all existing images in data dir
-    all_files = os.listdir(file_path)
-    jpg_files = [f for f in all_files if f.endswith('.jpg')]
-    with open('/logs/sent_plates.log', 'r') as f:
-        processed_files = [line.strip() for line in f]
-    for filename in sorted(jpg_files):
-        if filename not in processed_files:
-            print(f'alpr {flags} "{file_path}/{filename}" >> /logs/anpr_{LOG_NAME}.log')
-            cmd = f'alpr {flags} "{file_path}/{filename}" >> /logs/anpr_{LOG_NAME}.log'
-            subprocess.run(['sh', '-c', cmd])
-            with open('/logs/sent_plates.log', 'a') as f:
-                f.write(f'{filename}\n')
+    # # Process all existing images in data dir
+    # all_files = os.listdir(file_path)
+    # jpg_files = [f for f in all_files if f.endswith('.jpg')]
+    # with open('/logs/sent_plates.log', 'r') as f:
+    #     processed_files = [line.strip() for line in f]
+    # for filename in sorted(jpg_files):
+    #     if filename not in processed_files:
+    #         print(f'alpr {flags} "{file_path}/{filename}" >> /logs/anpr_{LOG_NAME}.log')
+    #         cmd = f'alpr {flags} "{file_path}/{filename}" >> /logs/anpr_{LOG_NAME}.log'
+    #         subprocess.run(['sh', '-c', cmd])
+    #         with open('/logs/sent_plates.log', 'a') as f:
+    #             f.write(f'{filename}\n')
 
     # Watch for new images in /data using inotify
     i = inotify.adapters.Inotify()
@@ -43,11 +43,10 @@ def main():
 
         # Process the new image file
         if 'IN_CREATE' in type_names and filename.endswith('.jpg'):
-            if filename not in processed_files:
-                cmd = f'alpr {flags} "{file_path}/{filename}" >> /logs/anpr_{LOG_NAME}.log'
-                subprocess.run(['sh', '-c', cmd])
-                with open('/logs/sent_plates.log', 'a') as f:
-                    f.write(f'{filename}\n')
+            cmd = f'alpr {flags} "{file_path}/{filename}" >> /logs/anpr_{LOG_NAME}.log'
+            subprocess.run(['sh', '-c', cmd])
+            with open('/logs/sent_plates.log', 'a') as f:
+                f.write(f'{filename}\n')
 
 if __name__ == '__main__':
     main()
