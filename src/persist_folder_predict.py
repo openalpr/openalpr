fe43eea451514c151ae0d04fc7968428f32195f3
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-03-22 22:33:41 -0300

    gfasgasdg

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -1,48 +1,71 @@
-import inotify.adapters
-import subprocess
-import os 
 import time
+import os
+import subprocess
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+class PlateProcessor(FileSystemEventHandler):
+    def __init__(self, dir_path, flags, log_name):
+        self.dir_path = dir_path
+        self.flags = flags
+        self.log_name = log_name
+
+    def on_created(self, event):
+        if event.is_directory:
+            return None
+
+        if not event.src_path.endswith('.jpg'):
+            return None
+
+        # Process the new image file
+        filename = os.path.basename(event.src_path)
+        cmd = f'echo {filename} >> /logs/sent_plates/{self.log_name}.log && alpr {self.flags} "{event.src_path}" >> /logs/processed_plates/{self.log_name}.log'
+        subprocess.run(['sh', '-c', cmd])
+        with open(f'/logs/sent_plates/{self.log_name}.log', 'a') as f:
+            f.write(f'{os.path.basename(event.src_path)}\n')
 
 def main():
     dir_path = '/detect'
     flags = '-c brg -p gn -j'
-    
+
     while not os.listdir(dir_path):
         time.sleep(0.5)
-    
+
     folders = sorted(next(os.walk(dir_path))[1])
-    LOG_NAME = folders[-1] if folders[-1] is not None else "default"
-    file_path = dir_path+'/'+LOG_NAME+"/crops/placa carro"
-    
-    # Create the processed images log file if it doesn't exist
-    if not os.path.exists(f'/logs/sent_plates_{LOG_NAME}.log'):
-        with open(f'/logs/sent_plates_{LOG_NAME}.log', 'w') as f:
+    log_name = folders[-1] if folders[-1] is not None else "default"
+    file_path = dir_path + '/' + log_name + "/crops/placa carro"
+
+    if not os.path.exists(f'/logs/sent_plates/{log_name}.log'):
+        with open(f'/logs/sent_plates/{log_name}.log', 'w') as f:
             pass
-        
+
     while not os.path.exists(file_path):
         time.sleep(0.5)
         
     # Process existing files in the directory
-    for filename in os.listdir(file_path):
+    for filename in sorted(os.listdir(file_path)):
         if filename.endswith('.jpg'):
-            cmd = f'alpr {flags} "{file_path}/{filename}" >> /logs/anpr_{LOG_NAME}.log'
+            f'echo {filename} >> /logs/sent_plates/{log_name}.log && alpr {flags} "{file_path}/{filename}" >> /logs/processed_plates/{log_name}.log'
             subprocess.run(['sh', '-c', cmd])
-            with open(f'/logs/sent_plates_{LOG_NAME}.log', 'a') as f:
+            with open(f'/logs/sent_plates/{log_name}.log', 'a') as f:
                 f.write(f'{filename}\n')
 
-    # Watch for new images in /data using inotify
-    i = inotify.adapters.Inotify()
-    i.add_watch(file_path)
 
-    for event in i.event_gen(yield_nones=False):
-        (_, type_names, path, filename) = event
+    # Watch for new images in /data using watchdog
+    event_handler = PlateProcessor(file_path, flags, log_name)
+    observer = Observer()
+    observer.schedule(event_handler, file_path, recursive=True)
+    observer.start()
 
-        # Process the new image file
-        if 'IN_CREATE' in type_names and filename.endswith('.jpg'):
-            cmd = f'alpr {flags} "{file_path}/{filename}" >> /logs/anpr_{LOG_NAME}.log'
-            subprocess.run(['sh', '-c', cmd])
-            with open(f'/logs/sent_plates_{LOG_NAME}.log', 'a') as f:
-                f.write(f'{filename}\n')
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
 
 if __name__ == '__main__':
+    time.sleep(5)
     main()
