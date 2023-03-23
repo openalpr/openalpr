9b79cc8c75cd7be2e91fb0cf5bfdea17c738ddbc
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-03-23 16:56:47 -0300

    improve file arct

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -6,8 +6,6 @@ from datetime import datetime
 
 def main():
     detect_dir = '/detect'
-    sent_plates_log_dir = '/logs/sent_plates'
-    processed_plates_log_dir = '/logs/processed_plates'
     flags = '-c brg -p gn -j '
     
     # Check if /detect is not empty
@@ -20,25 +18,28 @@ def main():
     category = 'placa carro'
     crops_dir = os.path.join(detect_dir, latest_folder, 'crops', category)
     sent_plates_file_dir = os.path.join(crops_dir, 'sent_plates')
+    
+    sent_plates_log_dir = f'/logs/{latest_folder}/sent_plates'
+    processed_plates_log_dir = f'/logs/{latest_folder}/processed_plates'
 
     # Wait for /crops/placa carro to exist
     while not os.path.exists(crops_dir):
         time.sleep(0.5)
         
     os.makedirs(sent_plates_file_dir, exist_ok=True)
-    os.makedirs(os.path.join(processed_plates_log_dir, latest_folder,category), exist_ok=True)
-    os.makedirs(os.path.join(sent_plates_log_dir, latest_folder,category), exist_ok=True)
+    os.makedirs(os.path.join(processed_plates_log_dir,category), exist_ok=True)
+    os.makedirs(os.path.join(sent_plates_log_dir,category), exist_ok=True)
         
-    if not os.path.exists(os.path.join(processed_plates_log_dir, latest_folder,category,'processed_plates.log')):
-        with open(os.path.join(processed_plates_log_dir, latest_folder,category, 'processed_plates.log'), 'w'):
+    if not os.path.exists(os.path.join(processed_plates_log_dir,category,'processed_plates.log')):
+        with open(os.path.join(processed_plates_log_dir,category, 'processed_plates.log'), 'w'):
             pass
         
-    if not os.path.exists(os.path.join(sent_plates_log_dir, latest_folder, category, 'sent_plates.log')):
-        with open(os.path.join(sent_plates_log_dir, latest_folder, category, 'sent_plates.log'), 'w'):
+    if not os.path.exists(os.path.join(sent_plates_log_dir, category, 'sent_plates.log')):
+        with open(os.path.join(sent_plates_log_dir, category, 'sent_plates.log'), 'w'):
             pass
         
-    sent_plates_log = os.path.join(sent_plates_log_dir, latest_folder, category, 'sent_plates.log')
-    processed_plates_log = os.path.join(processed_plates_log_dir, latest_folder,category, 'processed_plates.log')
+    sent_plates_log = os.path.join(sent_plates_log_dir, category, 'sent_plates.log')
+    processed_plates_log = os.path.join(processed_plates_log_dir,category, 'processed_plates.log')
     
     # Process each image in order
     while True:
