ca6c20466654463bfdecd9a81c7d99aa0b681fce
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-03-22 23:24:10 -0300

    dfgsdg

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -4,11 +4,14 @@ import subprocess
 import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from queue import Queue
+
 class PlateProcessor(FileSystemEventHandler):
     def __init__(self, dir_path, flags, log_name):
         self.dir_path = dir_path
         self.flags = flags
         self.log_name = log_name
+        self.queue = Queue()
 
     def on_created(self, event):
         if event.is_directory:
@@ -17,12 +20,26 @@ class PlateProcessor(FileSystemEventHandler):
         if not event.src_path.endswith('.jpg'):
             return None
 
-        # Process the new image file
-        filename = os.path.basename(event.src_path)
-        cmd = f'echo {filename} >> /logs/processed_plates/{self.log_name}.log && alpr {self.flags} "{event.src_path}" >> /logs/processed_plates/{self.log_name}.log'
-        subprocess.run(['sh', '-c', cmd])
-        with open(f'/logs/sent_plates/{self.log_name}.log', 'a') as f:
-            f.write(f'{os.path.basename(event.src_path)}\n')
+        # Add new image file to the queue
+        self.queue.put(event.src_path)
+
+    def process_queue(self):
+        while not self.queue.empty():
+            # Process the next image file in the queue
+            src_path = self.queue.get()
+            filename = os.path.basename(src_path)
+            cmd = f'echo {filename} >> /logs/processed_plates/{self.log_name}.log && alpr {self.flags} "{src_path}" >> /logs/processed_plates/{self.log_name}.log'
+            subprocess.run(['sh', '-c', cmd])
+            with open(f'/logs/sent_plates/{self.log_name}.log', 'a') as f:
+                f.write(f'{os.path.basename(src_path)}\n')
+
+    def on_modified(self, event):
+        if event.is_directory:
+            self.process_queue()
+
+    def on_moved(self, event):
+        if event.is_directory:
+            self.process_queue()
 
 def main():
     dir_path = '/detect'
@@ -41,7 +58,7 @@ def main():
 
     while not os.path.exists(file_path):
         time.sleep(0.5)
-        
+
     # Process existing files in the directory
     for filename in sorted(os.listdir(file_path)):
         if filename.endswith('.jpg'):
@@ -49,7 +66,6 @@ def main():
             subprocess.run(['sh', '-c', cmd])
             with open(f'/logs/sent_plates/{log_name}.log', 'a') as f:
                 f.write(f'{filename}\n')
-
 
     # Watch for new images in /data using watchdog
     event_handler = PlateProcessor(file_path, flags, log_name)
@@ -59,12 +75,12 @@ def main():
 
     try:
         while True:
+            event_handler.process_queue()  # Process the queue periodically
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
 
     observer.join()
-
 
 if __name__ == '__main__':
     time.sleep(5)
