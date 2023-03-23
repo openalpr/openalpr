45bb6e5974e9f4557d39edfd46de685c9fe61a9d
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-03-23 01:01:00 -0300

    dgasgd

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -1,79 +1,51 @@
-import time
 import os
-import subprocess
+import time
 import logging
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-from queue import Queue, Empty
-
-
-class PlateProcessor(FileSystemEventHandler):
-    def __init__(self, dir_path, flags, log_name, queue):
-        self.dir_path = dir_path
-        self.flags = flags
-        self.log_name = log_name
-        self.queue = queue
-
-    def on_created(self, event):
-        if event.is_directory:
-            return None
-
-        if not event.src_path.endswith('.jpg'):
-            return None
-
-        # Add the new image file to the queue
-        self.queue.put(event.src_path)
-
-
-def process_image(file_path, flags, log_name):
-    if not os.path.exists(f'/logs/sent_plates/{log_name}.log'):
-        with open(f'/logs/sent_plates/{log_name}.log', 'w') as f:
-            pass
-
-    filename = os.path.basename(file_path)
-    cmd = f'echo {filename} >> /logs/processed_plates/{log_name}.log && alpr {flags} "{file_path}" >> /logs/processed_plates/{log_name}.log'
-    subprocess.run(['sh', '-c', cmd])
-    with open(f'/logs/sent_plates/{log_name}.log', 'a') as f:
-        f.write(f'{filename}\n')
-
 
 def main():
-    dir_path = '/detect'
-    flags = '-c brg -p gn -j'
+    detect_dir = '/detect'
+    sent_plates_dir = '/logs/sent_plates'
+    processes_plates_dir = '/logs/processes_plates'
 
-    while not os.listdir(dir_path):
+    # Check if /detect is not empty
+    while not os.listdir(detect_dir):
         time.sleep(0.5)
 
-    folders = sorted(next(os.walk(dir_path))[1])
-    log_name = folders[-1] if folders[-1] is not None else "default"
-    file_path = dir_path + '/' + log_name + "/crops/placa carro"
+    # Get the latest folder in /detect
+    latest_folder = max(os.listdir(detect_dir), key=os.path.getctime)
+    crops_dir = os.path.join(detect_dir, latest_folder, 'crops', 'placa carro')
 
-    while not os.path.exists(file_path):
+    # Wait for /crops/placa carro to exist
+    while not os.path.exists(crops_dir):
         time.sleep(0.5)
 
-    # Create a queue to hold file paths of newly added images
-    queue = Queue()
+    # Process each image in order
+    processed_files = set()
+    while True:
+        # Get list of files in /crops/placa_carro
+        files = os.listdir(crops_dir)
+        files.sort()
 
-    # Watch for new images in /data using watchdog
-    event_handler = PlateProcessor(file_path, flags, log_name, queue)
-    observer = Observer()
-    observer.schedule(event_handler, file_path, recursive=True)
-    observer.start()
+        for filename in files:
+            # Skip files that have already been processed
+            if filename in processed_files:
+                continue
 
-    try:
-        while True:
-            try:
-                # Process the next image file in the queue
-                file_path = queue.get(timeout=1)
-                if file_path.endswith('.jpg'):
-                    process_image(file_path, flags, log_name)
-            except Empty:
-                pass
-    except KeyboardInterrupt:
-        observer.stop()
+            # Send file to OpenALPR and log sent file name
+            sent_filename = filename
+            sent_log_filename = os.path.join(sent_plates_dir, latest_folder + '.log')
+            with open(sent_log_filename, 'a') as f:
+                f.write(sent_filename + '\n')
 
-    observer.join()
+            # Process image with OpenALPR and log processing result
+            # Assuming OpenALPR script is called 'alpr'
+            alpr_output = os.popen('alpr -c brg -p gn -j' + os.path.join(crops_dir,sent_filename)).read()
+            processes_log_filename = os.path.join(processes_plates_dir, latest_folder + '.log')
+            with open(processes_log_filename, 'a') as f:
+                f.write(filename + '\n' + alpr_output + '\n')
 
+            # Add filename to processed set
+            processed_files.add(filename)
 
 if __name__ == '__main__':
     time.sleep(5)
