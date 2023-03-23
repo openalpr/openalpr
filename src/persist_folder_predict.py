d35ba3546961b85b5c92d89046540620db83de52
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-03-23 12:23:17 -0300

    plottwist

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -39,10 +39,14 @@ def main():
     while True:
         # Get list of files in /crops/placa_carro
         files = os.listdir(crops_dir)
-        logging.warning(files)
+        for filename in files:
+            logging.warn("fiiiilleeesssssss \n"+str(filename))
         files.sort()
 
         for filename in files:
+            
+            if not filename.endswith('.jpg'):
+                pass
             
             now = str(datetime.now())+" "
             processed_plates_log = os.path.join(processed_plates_log_dir, latest_folder+'.log')
