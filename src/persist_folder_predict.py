f0bfc7a42d10690231c100660153e7323fab7106
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-03-23 13:24:50 -0300

    fixed

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -18,23 +18,27 @@ def main():
     # Get the latest folder in /detect
     folders = sorted(next(os.walk(detect_dir))[1])
     latest_folder = folders[-1] if folders[-1] is not None else "default"
-    crops_dir = os.path.join(detect_dir, latest_folder, 'crops', 'placa carro')
+    category = 'placa carro'
+    crops_dir = os.path.join(detect_dir, latest_folder, 'crops', category)
     sent_plates_file_dir = os.path.join(crops_dir, 'sent_plates')
 
     # Wait for /crops/placa carro to exist
     while not os.path.exists(crops_dir):
         time.sleep(0.5)
         
-    if not os.path.exists(os.path.join(processed_plates_log_dir, latest_folder + '.log')):
-        with open(os.path.join(processed_plates_log_dir, latest_folder + '.log'), 'w'):
+    if not os.path.exists(os.path.join(processed_plates_log_dir, latest_folder, category + '.log')):
+        with open(os.path.join(processed_plates_log_dir, latest_folder, category + '.log'), 'w'):
             pass
         
-    if not os.path.exists(os.path.join(sent_plates_log_dir, latest_folder + '.log')):
-        with open(os.path.join(sent_plates_log_dir, latest_folder + '.log'), 'w'):
+    if not os.path.exists(os.path.join(sent_plates_log_dir, latest_folder, category + '.log')):
+        with open(os.path.join(sent_plates_log_dir, latest_folder, category + '.log'), 'w'):
             pass
-
+        
+    sent_plates_log = os.path.join(sent_plates_log_dir, latest_folder, category + '.log')
+    processed_plates_log = os.path.join(processed_plates_log_dir, latest_folder, category + '.log')
+    
     os.makedirs(sent_plates_file_dir, exist_ok=True)
-
+    
     # Process each image in order
     while True:
         # Get list of files in /crops/placa_carro
@@ -46,19 +50,18 @@ def main():
             if not filename.endswith('.jpg'):
                 continue
             
-            now = str(datetime.now())+" "
-            processed_plates_log = os.path.join(processed_plates_log_dir, latest_folder+'.log')
+            id = str(datetime.now()).replace(" ","").replace(":","").replace(".","").replace("-","")+"_"
             
             # Send file to OpenALPR and log sent file name
-            sent_log_filename = os.path.join(sent_plates_log_dir, latest_folder + '.log')
-            with open(sent_log_filename, 'a') as f:
-                f.write(now+ filename + '\n')
+            
+            with open(sent_plates_log, 'a') as f:
+                f.write(id + filename + '\n')
 
             # Process image with OpenALPR and log processing result
             # Assuming OpenALPR script is called 'alpr'
-            cmd = f'echo {now+filename} >> {processed_plates_log} && alpr {flags} "{os.path.join(crops_dir,filename)}" >> {processed_plates_log}'
+            cmd = f'echo {id + filename} >> {processed_plates_log} && alpr {flags} "{os.path.join(crops_dir,filename)}" >> {processed_plates_log}'
             subprocess.run(['sh', '-c', cmd])
-            shutil.move(os.path.join(crops_dir, filename), os.path.join(sent_plates_file_dir, (now+filename)))
+            shutil.move(os.path.join(crops_dir, filename), os.path.join(sent_plates_file_dir, (id + filename)))
 
 if __name__ == '__main__':
     time.sleep(5)
