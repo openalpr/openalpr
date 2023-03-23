2c11e708c0e19ccd64a609b1e14bfdb17bb610b1
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-03-22 23:38:42 -0300

    sdfsagas

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -4,14 +4,15 @@ import subprocess
 import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from queue import Queue
+from queue import Queue, Empty
+
 
 class PlateProcessor(FileSystemEventHandler):
-    def __init__(self, dir_path, flags, log_name):
+    def __init__(self, dir_path, flags, log_name, queue):
         self.dir_path = dir_path
         self.flags = flags
         self.log_name = log_name
-        self.queue = Queue()
+        self.queue = queue
 
     def on_created(self, event):
         if event.is_directory:
@@ -20,26 +21,21 @@ class PlateProcessor(FileSystemEventHandler):
         if not event.src_path.endswith('.jpg'):
             return None
 
-        # Add new image file to the queue
+        # Add the new image file to the queue
         self.queue.put(event.src_path)
 
-    def process_queue(self):
-        while not self.queue.empty():
-            # Process the next image file in the queue
-            src_path = self.queue.get()
-            filename = os.path.basename(src_path)
-            cmd = f'echo {filename} >> /logs/processed_plates/{self.log_name}.log && alpr {self.flags} "{src_path}" >> /logs/processed_plates/{self.log_name}.log'
-            subprocess.run(['sh', '-c', cmd])
-            with open(f'/logs/sent_plates/{self.log_name}.log', 'a') as f:
-                f.write(f'{os.path.basename(src_path)}\n')
 
-    def on_modified(self, event):
-        if event.is_directory:
-            self.process_queue()
+def process_image(file_path, flags, log_name):
+    if not os.path.exists(f'/logs/sent_plates/{log_name}.log'):
+        with open(f'/logs/sent_plates/{log_name}.log', 'w') as f:
+            pass
 
-    def on_moved(self, event):
-        if event.is_directory:
-            self.process_queue()
+    filename = os.path.basename(file_path)
+    cmd = f'echo {filename} >> /logs/processed_plates/{log_name}.log && alpr {flags} "{file_path}" >> /logs/processed_plates/{log_name}.log'
+    subprocess.run(['sh', '-c', cmd])
+    with open(f'/logs/sent_plates/{log_name}.log', 'a') as f:
+        f.write(f'{filename}\n')
+
 
 def main():
     dir_path = '/detect'
@@ -52,35 +48,32 @@ def main():
     log_name = folders[-1] if folders[-1] is not None else "default"
     file_path = dir_path + '/' + log_name + "/crops/placa carro"
 
-    if not os.path.exists(f'/logs/sent_plates/{log_name}.log'):
-        with open(f'/logs/sent_plates/{log_name}.log', 'w') as f:
-            pass
-
     while not os.path.exists(file_path):
         time.sleep(0.5)
 
-    # Process existing files in the directory
-    for filename in sorted(os.listdir(file_path)):
-        if filename.endswith('.jpg'):
-            cmd = f'echo {filename} >> /logs/processed_plates/{log_name}.log && alpr {flags} "{file_path}/{filename}" >> /logs/processed_plates/{log_name}.log'
-            subprocess.run(['sh', '-c', cmd])
-            with open(f'/logs/sent_plates/{log_name}.log', 'a') as f:
-                f.write(f'{filename}\n')
+    # Create a queue to hold file paths of newly added images
+    queue = Queue()
 
     # Watch for new images in /data using watchdog
-    event_handler = PlateProcessor(file_path, flags, log_name)
+    event_handler = PlateProcessor(file_path, flags, log_name, queue)
     observer = Observer()
     observer.schedule(event_handler, file_path, recursive=True)
     observer.start()
 
     try:
         while True:
-            event_handler.process_queue()  # Process the queue periodically
-            time.sleep(1)
+            try:
+                # Process the next image file in the queue
+                file_path = queue.get(timeout=1)
+                if file_path.endswith('.jpg'):
+                    process_image(file_path, flags, log_name)
+            except Empty:
+                pass
     except KeyboardInterrupt:
         observer.stop()
 
     observer.join()
+
 
 if __name__ == '__main__':
     time.sleep(5)
