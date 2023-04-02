5cb3337f5ec0f096d1312e637d7b22cc979dec4e
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-04-02 13:06:20 -0300

    typo

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -122,7 +122,7 @@ def main():
             time.sleep(0.5)
             continue
 
-        for category, file_list in files_cat_dict.item():
+        for category, file_list in files_cat_dict.items():
             for file in file_list:
                 process_image(
                     file,
