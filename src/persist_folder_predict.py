bd05bb01d15f10cd7e703bccd61f4efc86abae70
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-04-02 03:05:52 -0300

    change to do multicat preds

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -6,62 +6,134 @@ from datetime import datetime
 import time
 import os
 
-category = 'placa_carro'
 
 def clean_det_dir():
-    detect_dir = Path('/detect')
-    dest_dir = detect_dir / 'old'
-    items = [item for item in detect_dir.glob('*') if not os.path.samefile(item, dest_dir) and not os.path.commonpath([item, dest_dir]) == dest_dir]
+    detect_dir = Path("/detect")
+    dest_dir = detect_dir / "old"
+    items = [
+        item
+        for item in detect_dir.glob("*")
+        if not os.path.samefile(item, dest_dir)
+        and not os.path.commonpath([item, dest_dir]) == dest_dir
+    ]
     if items:
         for item in items:
             shutil.move(item, dest_dir)
 
-def process_image(image_path: Path, sent_plates_file_dir: Path, sent_plates_log_file: Path, processed_plates_log_dir: Path, flags: str):
-    id = str(datetime.now()).replace(" ","").replace(":","").replace(".","").replace("-","")
-    with sent_plates_log_file.open('a') as f:
-        f.write(id + '\n')
-    cmd = f'alpr {flags} {image_path} >> {processed_plates_log_dir / id}.log'
-    subprocess.run(['sh', '-c', cmd])
-    shutil.move(image_path, sent_plates_file_dir / (id+'.jpg'))
-    #logging.info(f"Processed {image_path.name} for category {category} with ID {id}")
+
+def process_image(
+    image_path: Path,
+    sent_plates_file_dir: Path,
+    sent_plates_log_file: Path,
+    processed_plates_log_dir: Path,
+    flags: str,
+    category: str,
+):
+    id = (
+        str(datetime.now())
+        .replace(" ", "")
+        .replace(":", "")
+        .replace(".", "")
+        .replace("-", "")
+    )
+    with sent_plates_log_file.open("a") as f:
+        f.write(id + "\n")
+    cmd = f"alpr {flags} {image_path} >> {processed_plates_log_dir / id}.log"
+    subprocess.run(["sh", "-c", cmd])
+    shutil.move(image_path, sent_plates_file_dir / (id + ".jpg"))
+    # logging.info(f"Processed {image_path.name} for category {category} with ID {id}")
+
 
 def main():
-    detect_dir = Path('/detect')
-    dest_dir = detect_dir / 'old'
-    flags = '-c brg -p gn -j'
+    categories = [
+        "placa_carro",
+        "placa_carro_mercosul",
+        "placa_moto",
+        "placa_moto_mercosul",
+    ]
+    detect_dir = Path("/detect")
+    dest_dir = detect_dir / "old"
+    flags_dict = {
+        categories[0]: ["-c brg -p gn -j"],
+        categories[1]: ["-c brg -p ms -j"],
+        categories[2]: ["-c brg2 -p gn -j"],
+        categories[3]: ["-c brg2 -p ms -j"],
+    }
 
-    while not [item for item in detect_dir.glob('*') if not os.path.samefile(item, dest_dir) and not os.path.commonpath([item, dest_dir]) == dest_dir]:
+    while not [
+        item
+        for item in detect_dir.glob("*")
+        if not os.path.samefile(item, dest_dir)
+        and not os.path.commonpath([item, dest_dir]) == dest_dir
+    ]:
         time.sleep(0.5)
 
-    latest_folder = sorted([item for item in detect_dir.glob('*') if not os.path.samefile(item, dest_dir) and not os.path.commonpath([item, dest_dir]) == dest_dir], key=lambda x: x.stat().st_mtime, reverse=True)[0].name
-    crops_dir = detect_dir / latest_folder / 'crops' / category
-    sent_plates_file_dir = crops_dir / 'sent_plates'
-    sent_plates_log_dir = Path('/logs') / latest_folder / 'sent_plates' / category
-    processed_plates_log_dir = Path('/logs') / latest_folder / 'processed_plates' / category
+    latest_folder = sorted(
+        [
+            item
+            for item in detect_dir.glob("*")
+            if not os.path.samefile(item, dest_dir)
+            and not os.path.commonpath([item, dest_dir]) == dest_dir
+        ],
+        key=lambda x: x.stat().st_mtime,
+        reverse=True,
+    )[0].name
 
-    crops_dir.mkdir(parents=True, exist_ok=True)
-    sent_plates_file_dir.mkdir(exist_ok=True)
-    sent_plates_log_dir.mkdir(parents=True, exist_ok=True)
-    processed_plates_log_dir.mkdir(parents=True, exist_ok=True)
+    empty_cat_dict = {
+        categories[0]: [],
+        categories[1]: [],
+        categories[2]: [],
+        categories[3]: [],
+    }
 
-    sent_plates_log_file = sent_plates_log_dir / 'sent_plates.log'
-    if not sent_plates_log_file.exists():
-        sent_plates_log_file.touch()
+    crops_dir_dict = empty_cat_dict
+    sent_plates_file_dir_dict = empty_cat_dict
+    sent_plates_log_dir_dict = empty_cat_dict
+    processed_plates_log_dir_dict = empty_cat_dict
+    sent_plates_log_file_dict = empty_cat_dict
+
+    for category in categories:
+        crops_dir_dict[category] = detect_dir / latest_folder / "crops" / category
+        sent_plates_file_dir_dict[category] = crops_dir_dict[category] / "sent_plates"
+        sent_plates_log_dir_dict[category] = (
+            Path("/logs") / latest_folder / "sent_plates" / category
+        )
+        processed_plates_log_dir_dict[category] = (
+            Path("/logs") / latest_folder / "processed_plates" / category
+        )
+        crops_dir_dict[category].mkdir(parents=True, exist_ok=True)
+        sent_plates_file_dir_dict[category].mkdir(exist_ok=True)
+        sent_plates_log_dir_dict[category].mkdir(parents=True, exist_ok=True)
+        processed_plates_log_dir_dict[category].mkdir(parents=True, exist_ok=True)
+        sent_plates_log_file_dict[category] = (
+            sent_plates_log_dir_dict[category] / "sent_plates.log"
+        )
+        if not sent_plates_log_file_dict[category].exists():
+            sent_plates_log_file_dict[category].touch()
 
     while True:
-        files = sorted(crops_dir.glob('*.jpg'))
-        for file in files:
-            process_image(file, sent_plates_file_dir, sent_plates_log_file, processed_plates_log_dir, flags)
-        else:
-            #logging.info(f"Finished processing all images in {crops_dir}")
-            
-            # Check for new files and process them
-            time.sleep(1)  # Wait for 1 second before checking again
-            new_files = sorted(crops_dir.glob('*.jpg'))
-            if new_files != files:
-                #logging.info("New files detected. Processing...")
-                files = new_files
-            
-if __name__ == '__main__':
+
+        files_cat_dict = empty_cat_dict
+
+        for category in categories:
+            files_cat_dict[category] = sorted(crops_dir_dict[category].glob("*.jpg"))
+
+        if not any(files_cat_dict.values()):
+            time.sleep(0.5)
+            continue
+
+        for category, file_list in files_cat_dict:
+            for file in file_list:
+                process_image(
+                    file,
+                    sent_plates_file_dir_dict[category],
+                    sent_plates_log_file_dict[category],
+                    processed_plates_log_dir_dict[category],
+                    flags_dict[category],
+                    category,
+                )
+
+
+if __name__ == "__main__":
     clean_det_dir()
     main()
