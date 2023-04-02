19af2343bebcd2da22095799e54bd489d2473681
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-04-02 13:24:23 -0300

    dsfa

--- src/persist_folder_predict.py
+++ src/persist_folder_predict.py
@@ -39,6 +39,7 @@ def process_image(
     with sent_plates_log_file.open("a") as f:
         f.write(id + "\n")
     cmd = f"alpr {flags} {image_path} >> {processed_plates_log_dir / id}.log"
+    print(cmd)
     subprocess.run(["sh", "-c", cmd])
     shutil.move(image_path, sent_plates_file_dir / (id + ".jpg"))
     # logging.info(f"Processed {image_path.name} for category {category} with ID {id}")
